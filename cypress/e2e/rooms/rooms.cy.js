c62521d736b66b6fe6f373d4540af2928988142f
braiancarrizo24 <braiancarrizo245@gmail.com>
2026-06-21 18:46:03 -0300

    Realice mi octavo caso de prueba

--- cypress/e2e/rooms/rooms.cy.js
+++ cypress/e2e/rooms/rooms.cy.js
@@ -57,7 +57,7 @@ describe ('Rooms',() =>{
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)')
     .and('contain','Today')
     .click()
-    cy.reserveOk('2026-08-01', '2026-08-07')
+    cy.reserveOk('2026-06-29', '2026-07-08')
     cy.get('.btn-primary')
     .should('be.visible')
     .and('contain','Reserve Now')
@@ -68,17 +68,29 @@ describe ('Rooms',() =>{
 
    it('Verificacion de reserva fallida con datos invalidos',()=> {
     cy.navigate()
-    cy.get('.rbc-day-bg')
-    cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)')
-    .and('contain','Today')
+    cy.get('.rbc-day-bg').scrollIntoView()
+    cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)').scrollIntoView()
+    .and('contain','Today').scrollIntoView()
     .click()
-    cy.reserveDateFail('2026-08-01', '2026-08-07')
+    cy.reserveDateFail('2026-06-29', '2026-07-08')
     cy.get('.btn-primary')
     .should('be.visible')
     .and('contain','Reserve Now')
     .click()
 
   })
+
+it ('Verificacion de seleccion de otras alternativas de rooms', () => {
+     cy.navigate()
+     cy.get('.container > .fs-4').scrollIntoView()
+     .and('contain','Similar Rooms You Might Like')
+     cy.get(':nth-child(1) > .card > .card-body > .card-title')
+     .and('contain','Double')
+     cy.get(':nth-child(1) > .card > .card-body > .btn')
+     .and('contain','View Details')
+     .click()
+  })
+
 
     
      
