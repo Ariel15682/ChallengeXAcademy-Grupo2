10551b7fc8a65bede1ec2f1eae7b8398bfcc3944
braiancarrizo24 <braiancarrizo245@gmail.com>
2026-06-21 18:06:46 -0300

    Añadi un nuevo comando para reserva con datos validos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,5 @@
-Cypress.Commands.add('accederPaginaAdmin', () => {
-  cy.visit('https://automationintesting.online/admin/')
-})
-
 Cypress.Commands.add('navigate', () => {
+  cy.visit('https://automationintesting.online')
   cy.get(':nth-child(1) > .nav-link').click()
 
   cy.get('.row > :nth-child(1) > .card > .card-body > .card-title')
@@ -11,4 +8,36 @@ Cypress.Commands.add('navigate', () => {
 
   cy.get(':nth-child(1) > .card > .card-footer > .btn')
     .click()
+  cy.get('.rbc-calendar')
+})
+
+Cypress.Commands.add('reserveOk', (checkin, checkout) => {
+
+  cy.fixture('registerNewReservation').then((data) => {
+
+    cy.visit(
+      `https://automationintesting.online/reservation/1?checkin=${checkin}&checkout=${checkout}`
+    )
+
+    cy.get('#doReservation')
+      .should('be.visible')
+      .and('contain', 'Reserve Now')
+      .click()
+
+    cy.get('[name="firstname"]')
+      .clear()
+      .type(data.reservationRegisterOk.Name)
+
+    cy.get('[name="lastname"]')
+      .clear()
+      .type(data.reservationRegisterOk.LastName)
+
+    cy.get('[name="email"]')
+      .clear()
+      .type(data.reservationRegisterOk.email)
+
+    cy.get('[name="phone"]')
+      .clear()
+      .type(data.reservationRegisterOk.phone)
+  })
 })
