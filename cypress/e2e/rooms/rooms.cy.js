4a0696ad2d3c132e693f55ad16ba12387f8760fb
braiancarrizo24 <braiancarrizo245@gmail.com>
2026-06-21 18:05:04 -0300

    Avance con mis casos de prueba y modifique el archivo registerNewReservation

--- cypress/e2e/rooms/rooms.cy.js
+++ cypress/e2e/rooms/rooms.cy.js
@@ -9,47 +9,50 @@ describe ('Rooms',() =>{
 
   it ('Verificacion del boton "Today" en single room', () => {
      cy.navigate()
-     cy.get('.rbc-toolbar')
      cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)').should('be.visible').and('contain', 'Today').click()
   })
   
   it('Verificacion del boton "Back" en single room', () => {
     cy.navigate()
-    cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(2)').should('be.visible').and('contain', 'Back').click()  
  })
 
   it('Verificacion del boton "Next" en single room',() => {
     cy.navigate()
-    cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(3)').should('be.visible').and('contain', 'Next').click()
   })
   
-  it('Verificacion de reserva fallida de single rooms',()=>{
+  it('Verificacion de reserva fallida de single rooms, mes anterior',()=>{
     cy.navigate()
-    cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(2)').should('be.visible').and('contain', 'Back').click()
     cy.get('.rbc-calendar')
     cy.get('.rbc-day-bg')
-    cy.contains('.rbc-button-link', '01')
-    cy.get('.rbc-day-bg')
-    cy.contains('.rbc-button-link', '07')
-    cy.get('.rbc-month-row')
-  
+    cy.reserveOk('2026-05-01', '2026-05-07')
+    cy.get('.btn-primary')
+    .should('be.visible')
+    .and('contain','Reserve Now')
+    .click()
     
   })
   
-  it('Verificacion de reserva fallida de single rooms')
-    
+  it('Verificacion de reserva fallida de single rooms en dias pasados',()=> {
     cy.navigate()
     cy.get('.rbc-toolbar')
     cy.get('.rbc-calendar')
     cy.get('.rbc-day-bg')
-    cy.contains('.rbc-button-link', '01')
-    cy.get('.rbc-day-bg')
-    cy.contains('.rbc-button-link', '07')
-    cy.get('.rbc-month-row')
+    cy.reserveOk('2026-06-01', '2026-06-07')
+    cy.get('.btn-primary')
+    .should('be.visible')
+    .and('contain','Reserve Now')
+    .click()
+    
+    
+  })
+
   
+
+    
+     
   
 
 
